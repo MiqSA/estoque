16182749c2921007ed2855c96e541e93dc061d8c
MiqSA <eng.miqueias.sa@gmail.com>
2020-05-28 11:06:43 -0300

    Adicionei configuração de banco de dados

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ from config import app_config, app_active
 
 config = app_config[app_active]
 
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app(config_name):
     app = Flask(__name__, template_folder='templates')
     app.secret_key = config.SECRET
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(config.APP)
+    db.init_app(app)
 
     @app.route('/')
     def index():
--- config.py
+++ config.py
@@ -8,6 +8,14 @@ class Config(object):
     TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     APP = None
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://pythonuser:pass@localhost:3306/livro_flask'
+    #Preencha com os dados do seu banco de dados
+    # User - Usuário do banco # Passwd - Senha do usuário
+    # Host - Geralmente no local fica localhost
+    # Port - Geralmente 3306 no mysql
+    # Database - Nome do banco de dados
+
+
 
 
 class DevelopmentConfig(Config):
